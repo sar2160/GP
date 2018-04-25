eee7f4d73a2dc19404166ed0fa664356b2761268
sar2160 <srimmele@gmail.com>
2018-04-25 04:11:54 +0000

    working hacky partials

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import gpflow
+from partial_vgp import PartialVGP
 
 
 def Poisson_Model(X, y, use_priors = False, e_s = 0, period = 12):
@@ -82,6 +83,38 @@ def LongTerm_Model(X, y, use_priors = False, e_s = 0, period = 12):
 
 
             m = gpflow.models.VGP(X, y, full_kern,  likelihood = like, mean_function = None)
+
+            m.kern.periodic.period = period
+            m.kern.periodic.period.trainable = True
+
+            Tprior = gpflow.priors.StudentT(mean = 0 , scale = 1, deg_free = 4)
+
+            if use_priors:
+                m.kern.rbf.variance.prior      = Tprior
+                m.kern.periodic.variance.prior = Tprior
+                m.kern.linear.variance.prior   = Tprior
+                
+                m.kern.rbf.lengthscales.prior = Tprior
+                m.kern.periodic.lengthscales.prior = Tprior
+                
+            return m
+
+
+def LongTermPartial_Model(X, y, use_priors = False, e_s = 0, period = 12):
+
+    with gpflow.defer_build():
+
+            like = gpflow.likelihoods.Poisson(binsize = e_s)
+
+            kern_t_effect = gpflow.kernels.RBF(1, active_dims=[0], name='time_effect')
+            ## Will have to write custom kernel to match Flaxman 2014
+            kern_p_effect = gpflow.kernels.Periodic(1, active_dims=[0], name = 'periodic_effect')
+            kern_l_effect = gpflow.kernels.Linear(1, active_dims = [0], name = 'linear_effect', variance = 1.0)
+
+            full_kern =  kern_t_effect + kern_p_effect + kern_l_effect
+
+
+            m = PartialVGP(X, y, full_kern,  likelihood = like, mean_function = None)
 
             m.kern.periodic.period = period
             m.kern.periodic.period.trainable = True
--- partial_vgp.py
+++ partial_vgp.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
 
-from gpflow.vgp import VGP
-from gpflow.param import AutoFlow
-from gpflow._settings import settings
+from gpflow.models.vgp import VGP
+from gpflow.decors import autoflow, params_as_tensors
+from gpflow import settings
+from gpflow.conditionals import conditional
 
-float_type = settings.dtypes.float_type
+
 
 
 class PartialVGP(VGP):
@@ -14,12 +15,23 @@ class PartialVGP(VGP):
         if K_part is None:
             K_part = self.kern
         K = K_part
-
-        mu, var = conditional(Xnew, self.X, self.kern, self.q_mu,
+    
+            
+        mu, var = conditional(Xnew, self.X, K, self.q_mu,
                               q_sqrt=self.q_sqrt, full_cov=full_cov, white=True)
         return mu + self.mean_function(Xnew), var
+    
+    @autoflow((settings.float_type, [None, None]))
+    def predict_f_linear(self, Xnew, full_cov=False):
+        kern = self.kern.linear
+        return self._build_predict(Xnew, full_cov, kern)
 
+    @autoflow((settings.float_type, [None, None]))
+    def predict_f_periodic(self, Xnew, full_cov=False):
+        kern = self.kern.periodic
+        return self._build_predict(Xnew, full_cov, kern)
 
-    @AutoFlow(Xnew=(float_type, [None, None]))
-    def predict_f_partial(self, Xnew, full_cov=False, kern=None):
-        return self.build_predict(Xnew, full_cov, kern)
+    @autoflow((settings.float_type, [None, None]))
+    def predict_f_rbf(self, Xnew, full_cov=False):
+        kern = self.kern.rbf
+        return self._build_predict(Xnew, full_cov, kern)
