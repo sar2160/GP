0d75dfc3ea039302b796c69045ddcd335fe7fd99
sar2160 <srimmele@gmail.com>
2018-04-28 20:43:11 +0000

    more models

--- models.py
+++ models.py
@@ -29,13 +29,13 @@ def Poisson_Model(X, y, use_priors = False, e_s = 0, period = 12):
                 m.kern.periodic.variance.prior = normal_prior
                 m.kern.rbf_2.variance.prior    = normal_prior
 
-                m.kern.rbf_1.lengthscales.prior = normal_prior
-                m.kern.rbf_2.lengthscales.prior = normal_prior
-                m.kern.periodic.lengthscales.prior = normal_prior
+                #m.kern.rbf_1.lengthscales.prior = normal_prior
+                #m.kern.rbf_2.lengthscales.prior = normal_prior
+                #m.kern.periodic.lengthscales.prior = normal_prior
 
             return m
 
-def Matern32_Model(X, y, use_priors = False, e_s = 0, period = 12 ):
+def Matern12_Model(X, y, use_priors = False, e_s = 0, period = 12 ):
 
     with gpflow.defer_build():
 
@@ -62,7 +62,40 @@ def Matern32_Model(X, y, use_priors = False, e_s = 0, period = 12 ):
                 m.kern.periodic.variance.prior = t_prior
                 m.kern.rbf.variance.prior    = t_prior
 
-                m.kern.matern12.lengthscales.prior = t_prior
+                #m.kern.matern12.lengthscales.prior = t_prior
+                #m.kern.rbf.lengthscales.prior = t_prior
+                #m.kern.periodic.lengthscales.prior = t_prior
+
+    return m
+
+def SafeMatern32_Model(X, y, use_priors = False, e_s = 0, period = 12 ):
+
+    with gpflow.defer_build():
+
+        like = gpflow.likelihoods.Poisson(binsize = e_s)
+        kern_s_effect = gpflow.kernels.SafeMatern32(input_dim =  2, active_dims=[1,2], name='space_effect')
+        kern_t_effect = gpflow.kernels.RBF(1, active_dims=[0], name='time_effect')
+
+        ## Will have to write custom kernel to match Flaxman 2014
+        kern_p_effect = gpflow.kernels.Periodic(1, active_dims=[0], name = 'periodic_effect')
+        kern_st_effect = gpflow.kernels.Product([kern_s_effect ,kern_t_effect])
+
+        full_kern =  kern_t_effect + kern_s_effect + kern_p_effect + kern_st_effect
+
+        m = gpflow.models.VGP(X, y, full_kern,  likelihood = like, mean_function = None)
+
+        m.kern.periodic.period = period
+        m.kern.periodic.period.trainable = True
+
+
+        t_prior = gpflow.priors.StudentT(mean = 0 , scale = 5, deg_free = 4)
+
+        if use_priors:
+                m.kern.safematern32.variance.prior    = t_prior
+                m.kern.periodic.variance.prior = t_prior
+                m.kern.rbf.variance.prior    = t_prior
+
+                m.kern.safematern32.lengthscales.prior = t_prior
                 m.kern.rbf.lengthscales.prior = t_prior
                 m.kern.periodic.lengthscales.prior = t_prior
 
@@ -74,8 +107,8 @@ def All_Matern_Model(X, y, use_priors = False, e_s = 0, period = 12 ):
     with gpflow.defer_build():
 
         like = gpflow.likelihoods.Poisson(binsize = e_s)
-        kern_s_effect = gpflow.kernels.Matern52(input_dim =  2, active_dims=[1,2], name='space_effect')
-        kern_t_effect = gpflow.kernels.Matern32(1, active_dims=[0], name='time_effect')
+        kern_s_effect = gpflow.kernels.SafeMatern32(input_dim =  2, active_dims=[1,2], name='space_effect')
+        kern_t_effect = gpflow.kernels.Matern52(1, active_dims=[0], name='time_effect')
 
         ## Will have to write custom kernel to match Flaxman 2014
         kern_p_effect = gpflow.kernels.Periodic(1, active_dims=[0], name = 'periodic_effect')
@@ -92,13 +125,13 @@ def All_Matern_Model(X, y, use_priors = False, e_s = 0, period = 12 ):
         t_prior = gpflow.priors.StudentT(mean = 0 , scale = 1, deg_free = 4)
 
         if use_priors:
-                #m.kern.matern32.variance.prior    = t_prior
+                m.kern.safematern32.variance.prior    = t_prior
                 m.kern.periodic.variance.prior = t_prior
-                #m.kern.matern52.variance.prior    = t_prior
+                m.kern.matern52.variance.prior    = t_prior
 
-                #m.kern.matern32.lengthscales.prior = t_prior
+                #m.kern.safematern32.lengthscales.prior = t_prior
                 #m.kern.matern52.lengthscales.prior = t_prior
-                m.kern.periodic.lengthscales.prior = t_prior
+                #m.kern.periodic.lengthscales.prior = t_prior
 
     return m
 
@@ -184,17 +217,44 @@ def Poisson_Model_T(X, y, use_priors = False, e_s = 0, period = 12):
             m = gpflow.models.VGP(X, y, full_kern,  likelihood = like, mean_function = None)
 
             m.kern.periodic.period = period
-            m.kern.periodic.period.trainable = True
+            m.kern.periodic.period.trainable = False
 
-            t_prior = gpflow.priors.StudentT(mean = 0 , scale = 1, deg_free = 10)
+            t_prior = gpflow.priors.StudentT(mean = 0 , scale = 1, deg_free = 4)
 
             if use_priors:
                 m.kern.rbf_1.variance.prior    = t_prior
                 m.kern.periodic.variance.prior = t_prior
                 m.kern.rbf_2.variance.prior    = t_prior
 
+                #m.kern.rbf_1.lengthscales.prior = t_prior
+                #m.kern.rbf_2.lengthscales.prior = t_prior
+                #m.kern.periodic.lengthscales.prior = t_prior
+
+            return m
+        
+        
+def ST_Model(X, y, use_priors = False, e_s = 0):
+
+    with gpflow.defer_build():
+
+            like = gpflow.likelihoods.Poisson(binsize = e_s)
+
+            kern_s_effect = gpflow.kernels.RBF(2, active_dims=[1,2], name='space_effect')
+            kern_t_effect = gpflow.kernels.RBF(1, active_dims=[0], name='time_effect')
+
+            full_kern =  kern_t_effect + kern_s_effect
+
+            m = gpflow.models.VGP(X, y, full_kern,  likelihood = like, mean_function = None)
+
+ 
+
+            t_prior = gpflow.priors.StudentT(mean = 0 , scale = 1, deg_free = 4)
+
+            if use_priors:
+                m.kern.rbf_1.variance.prior    = t_prior
+                m.kern.rbf_2.variance.prior    = t_prior
+
                 m.kern.rbf_1.lengthscales.prior = t_prior
                 m.kern.rbf_2.lengthscales.prior = t_prior
-                m.kern.periodic.lengthscales.prior = t_prior
 
             return m
--- utils.py
+++ utils.py
@@ -25,6 +25,11 @@ def preprocess(df, start_date, training_end_date, testing_end_date = None, train
     else:
         test = df.loc[training_end_date:].copy()
     
+    test.drop(training_end_date, inplace = True) # drop the first week still in training period
+    
+    reindex = train.DATE_IND.min()
+    train.DATE_IND -= reindex
+    test.DATE_IND  -=  reindex
     y_train = train.COUNT.values.reshape((len(train),1))
 
     y_test = test.COUNT.values.reshape((len(test),1))
